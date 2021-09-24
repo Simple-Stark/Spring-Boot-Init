cd4435ff6b62e8069182989bebd38c65cc686ba1
Simple <wrh_1125@163.com>
2021-09-24 17:37:45 +0800

    feat(添加日志打印功能): 添加日志打印功能

--- src/main/java/com/simple/init/controller/TestController.java
+++ src/main/java/com/simple/init/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.simple.init.controller;
 
 import com.simple.common.result.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Simple
  * @date 2021/9/22 22:47
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
     public Result<String> hello() {
+        log.info("log test");
         return Result.success("hello init, hello 自定义common包");
     }
 
