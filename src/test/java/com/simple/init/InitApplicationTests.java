422d73acbe7b05f262a0042a4f3cbb005a8cbe34
Simple <wrh_1125@163.com>
2021-09-26 17:40:49 +0800

    test(测试): MybatisPlus 自动生成器测试

--- src/test/java/com/simple/init/InitApplicationTests.java
+++ src/test/java/com/simple/init/InitApplicationTests.java
@@ -1,13 +1,33 @@
 package com.simple.init;
 
+import com.simple.common.utils.MyBatisPlusGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class InitApplicationTests {
+
+    /**
+     * 代码自定义生成器
+     */
+    @Resource
+    MyBatisPlusGenerator myBatisPlusGenerator;
 
     @Test
     void contextLoads() {
     }
 
+    /**
+     * 自动生成代码
+     */
+    @Test
+    void mybatisPlus() {
+        List<String> tableList = new ArrayList<>();
+        tableList.add("user");
+        myBatisPlusGenerator.generator(tableList,"Simple","com.simple.init");
+    }
 }
